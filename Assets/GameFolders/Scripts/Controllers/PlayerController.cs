4af308425902752f5b4442a837469b8bf066e74f
ugurhattat <ugurhattat16@gmail.com>
2025-07-03 17:30:20 +0300

    Encapsulation and Field usage in the project

--- Assets/GameFolders/Scripts/Controllers/PlayerController.cs
+++ Assets/GameFolders/Scripts/Controllers/PlayerController.cs
@@ -2,7 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace OopProject1.Controllers
 {
 
+    
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] float jumpForce = 500f;
+        //field private bir field'dir.
+        Rigidbody2D _rigidbody2D;
+
+        private void Awake()
+        {
+           _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _rigidbody2D.AddForce(Vector2.up * jumpForce);
+            }
+        }
+    }
 }
+
