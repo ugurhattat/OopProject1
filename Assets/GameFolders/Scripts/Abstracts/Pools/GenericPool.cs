2a03f17759d8b0486cb3a70849011fcedd1d3d77
ugurhattat <ugurhattat16@gmail.com>
2025-09-07 21:10:27 +0300

    feat: implement generic object pooling system

---
+++ Assets/GameFolders/Scripts/Abstracts/Pools/GenericPool.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OopProject1.Abstracts.Pools
+{
+    public abstract class GenericPool<T> : MonoBehaviour where T : Component
+    {
+        [SerializeField] T[] prefabs;
+        [SerializeField] int countLoop = 5;
+
+        Queue<T> _poolPrefabs = new Queue<T>();
+
+        private void Awake()
+        {
+            SingletonObject();
+        }
+
+        private void Start()
+        {
+            GrowPoolPrefab();
+        }
+
+        protected abstract void SingletonObject();
+
+        public T Get()
+        {
+            if (_poolPrefabs.Count == 0)
+            {
+                GrowPoolPrefab();
+            }
+
+            return _poolPrefabs.Dequeue();
+        }
+
+        private void GrowPoolPrefab()
+        {
+            for (int i = 0; i < countLoop; i++)
+            {
+                T newPrefab = Instantiate(prefabs[Random.Range(0, prefabs.Length)]);
+                newPrefab.gameObject.SetActive(false);
+                _poolPrefabs.Enqueue(newPrefab);
+            }
+        }
+
+        public void Set(T poolObject)
+        {
+            poolObject.gameObject.SetActive(false);
+            _poolPrefabs.Enqueue(poolObject);
+        }
+    }
+}
+
+
