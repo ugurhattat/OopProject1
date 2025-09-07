de7f3c44b2dcb1685a7605fda51f01e399e3e0bb
ugurhattat <ugurhattat16@gmail.com>
2025-09-07 17:47:56 +0300

    feat: add background music and persistent object management

---
+++ Assets/GameFolders/Scripts/Concretes/Cores/PersistentObject.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OopProject1.Cores
+{
+    public class PersistentObject : MonoBehaviour
+    {
+        [SerializeField] GameObject persistentPrefab;
+
+        static bool _isFirstTime = true;
+        private void Start()
+        {
+            if (_isFirstTime)
+            {
+                SpawnPersistentObjects();
+                _isFirstTime = false;
+
+            }
+        }
+
+        private void SpawnPersistentObjects()
+        {
+            GameObject newObject = Instantiate(persistentPrefab);
+            DontDestroyOnLoad(newObject);
+        }
+    }
+}
+
+
